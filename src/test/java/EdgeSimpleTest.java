7adf7a22fccf596ea3b7bea80af46ddb754a3bfb
Tomas David <tdavid@redhat.com>
2017-06-26 15:30:18 +0200

    modify test to reproduce edge bug

--- src/test/java/EdgeSimpleTest.java
+++ src/test/java/EdgeSimpleTest.java
@@ -1,37 +1,51 @@
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class EdgeSimpleTest {
 
+    private static final String EDGE_WEBDRIVER_PROPERTY = "webdriver.edge.driver";
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty(EDGE_WEBDRIVER_PROPERTY, "C:\\Selenium\\MicrosoftWebDriver.exe");
+    }
+
     @Test
     public void searchDogOnGoogleTest() throws InterruptedException {
+        driver = new FirefoxDriver();
 
-        System.setProperty("webdriver.edge.driver", "C:\\Selenium\\MicrosoftWebDriver.exe");
-        WebDriver driver = new EdgeDriver();
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("index.html").getFile());
 
-        driver.get("http://www.google.com");
+        driver.get("file://" + file.getAbsolutePath());
 
-        WebElement searchBox = driver.findElement(By.id("lst-ib"));
+        WebElement firstDiv = driver.findElement(By.className("first"));
+        Assert.assertTrue(firstDiv.getText().equals("Hello world!"));
 
-        searchBox.sendKeys("Dog");
+        WebElement addDivButton = driver.findElement(By.className("button"));
+        addDivButton.click();
 
-        Thread.sleep(1000);
-
-        System.out.println("Page title is: " + driver.getTitle());
-
-        driver.quit();
+        WebElement secondDiv = driver.findElement(By.className("second"));
+        Assert.assertTrue(secondDiv.getText().equals("Goodbye world!"));
     }
 
-    @Test
-    public void openGoogleTest() throws InterruptedException {
-        System.setProperty("webdriver.edge.driver", "C:\\Selenium\\MicrosoftWebDriver.exe");
-        EdgeDriver driver = new EdgeDriver();
-        driver.get("http://www.google.com");
-        Thread.sleep(1000);
-        driver.quit();
+    @After
+    public void clean() {
+        System.clearProperty(EDGE_WEBDRIVER_PROPERTY);
+        if (driver != null) {
+            driver.quit();
+        }
     }
+
 }
