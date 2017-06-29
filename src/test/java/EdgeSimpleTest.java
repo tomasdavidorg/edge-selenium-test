1768ce900af7b833b54d33c915aad57077275c6e
Tomas David <tdavid@redhat.com>
2017-06-29 07:46:34 +0200

    arq bug test

--- src/test/java/EdgeSimpleTest.java
+++ src/test/java/EdgeSimpleTest.java
@@ -1,30 +1,33 @@
 import java.io.File;
 
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+@RunWith(Arquillian.class)
 public class EdgeSimpleTest {
 
-    private static final String EDGE_WEBDRIVER_PROPERTY = "webdriver.edge.driver";
+//    private static final String EDGE_WEBDRIVER_PROPERTY = "webdriver.edge.driver";
 
+    @Drone
     private WebDriver driver;
 
     @Before
     public void setUp() {
-        System.setProperty(EDGE_WEBDRIVER_PROPERTY, "C:\\Selenium\\MicrosoftWebDriver.exe");
+//        System.setProperty(EDGE_WEBDRIVER_PROPERTY, "C:\\Selenium\\MicrosoftWebDriver.exe");
     }
 
     @Test
     public void searchDogOnGoogleTest() throws InterruptedException {
-        driver = new FirefoxDriver();
-
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("index.html").getFile());
 
@@ -42,10 +45,10 @@ public class EdgeSimpleTest {
 
     @After
     public void clean() {
-        System.clearProperty(EDGE_WEBDRIVER_PROPERTY);
-        if (driver != null) {
-            driver.quit();
-        }
+//        System.clearProperty(EDGE_WEBDRIVER_PROPERTY);
+//        if (driver != null) {
+//            driver.quit();
+//        }
     }
 
 }
