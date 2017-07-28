40fbf4f9d5b1a15ad1b0ecbe39bb883ab9a5517c
Tomas David <tdavid@redhat.com>
2017-07-28 15:45:44 +0200

    change to bootstrap reproducer

--- src/test/java/org/tomasdavid/selenium/test/edge/EdgeSimpleTest.java
+++ src/test/java/org/tomasdavid/selenium/test/edge/EdgeSimpleTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class EdgeSimpleTest {
 
-    private static final String DROOLS_WB_URL = "http://kieci-02.lab.eng.brq.redhat.com:32867/kie-drools-wb/kie-drools-wb.jsp";
+    private static final String TESTING_HTML_FILE = "index.html";
 
     private WebDriver driver;
 
@@ -22,30 +22,15 @@ public class EdgeSimpleTest {
 
     @Test
     public void searchDogOnGoogleTest() throws InterruptedException {
-        driver.get(DROOLS_WB_URL);
+        String htmlFilePath = getClass().getClassLoader().getResource(TESTING_HTML_FILE).getPath();
+        driver.get("file://" + htmlFilePath);
 
-        // login into kie-wb
-        WebElement name = driver.findElement(By.cssSelector("input[name='j_username']"));
-        name.sendKeys("admin");
-        WebElement pass = driver.findElement(By.cssSelector("input[name='j_password']"));
-        pass.sendKeys("admin");
-        WebElement button = driver.findElement(By.cssSelector("input.button"));
-        button.click();
-        Thread.sleep(15000);
+        driver.findElement(By.id("openDialogButton")).click();
+        Thread.sleep(1000);
+        WebElement testingInput = driver.findElement(By.id("testingInput"));
+        testingInput.sendKeys("Hello");
 
-        // Open Project Authoring - Teams
-        WebElement tableWithLinks = driver.findElement(By.cssSelector("table.well"));
-        tableWithLinks.findElement(By.linkText("Project Authoring")).click();
-        Thread.sleep(1000);
-        driver.findElement(By.linkText("Teams")).click();
-        Thread.sleep(1000);
-        driver.findElement(By.cssSelector("button.btn-primary")).click();
-        Thread.sleep(1000);
-
-        // interact with name input
-        WebElement nameInput = driver.findElement(By.cssSelector("input[data-field='name']"));
-        nameInput.sendKeys("MyTeam");
-        nameInput.clear(); //org.openqa.selenium.ElementNotVisibleException: Element not displayed
+        testingInput.clear(); // org.openqa.selenium.ElementNotVisibleException:
     }
 
     @After
